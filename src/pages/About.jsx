41fd60111dfd380baf674dd49ad1836254b842a8
Jovany Hochart <jovanyhochart@gmail.com>
2025-02-01 16:04:57 +0100

    update about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,10 +8,10 @@ function About() {
             <h1>À propos de ce site...</h1>
             <p>
                 Ce site est une application permettant de suivre l'évolution de vos candidatures. <br/><br/>
-                Cela peut être utilisé pour différents contextes comme des candidatures à des emplois, des écoles, des formations... <br/><br/>
-                Vous pouvez créer votre candidature en saisissant un nom correspondant, une date où vous avez postulé, le lieu où ça se passe et le lien pour postuler. <br/><br/>
-                Vous pouvez ensuite ranger chaque candidature dans des colonnes selon l'état de la réponse en les glissant à votre guise, ou même les supprimer. <br/><br/>
-                Vos candidatures seront ensuite sauvegardés en local sur votre appareil !
+                Elle peut être utilisée dans différents contextes, tels que les candidatures à des emplois, des écoles, des formations, etc. <br/><br/>
+                Vous pouvez créer une candidature en saisissant le nom de la structure, l'objet de la candidature, une date de candidature, le lieu de l'offre et le lien pour postuler. <br/><br/>
+                Ensuite, vous pouvez organiser chaque candidature dans des colonnes en fonction de l'état de la réponse, en les déplaçant à votre guise, ou même les supprimer. <br/><br/>
+                Vos candidatures seront ensuite sauvegardées en local sur votre appareil !
             </p>
             <p>Auteur : Jovany Hochart</p>
         </div>
